ad32dc801672f7739f1a84f0a5401c8f98d11993
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-14 11:53:00 -0300

    feat: implementada função de login na api
    Requisito 3 concluído

--- src/api.js
+++ src/api.js
@@ -1,13 +1,14 @@
 const express = require('express');
 require('express-async-errors');
 
+const authRouter = require('./routers/authRouter');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.use('/login', authRouter);
 
 app.use(errorMiddleware);
 // É importante exportar a constante `app`,
