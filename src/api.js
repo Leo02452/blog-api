804298e7c2c7453a528d213f53429d95b92f4fc9
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-14 11:42:50 -0300

    feat: criado e implementado middleware de erro

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
+require('express-async-errors');
 
-// ...
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.json());
 
 // ...
 
+app.use(errorMiddleware);
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
---
+++ src/middlewares/errorMiddleware.js
@@ -0,0 +1,14 @@
+const errorMiddleware = (err, _req, res, _next) => {
+  switch (err.name) {
+    case 'UnauthorizedError':
+      res.status(400).json({ message: err.message });
+      break;
+    case 'ValidationError':
+      res.status(err.code).json({ message: err.message });
+      break;
+    default:
+      res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = errorMiddleware;
