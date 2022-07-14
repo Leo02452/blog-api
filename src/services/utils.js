a5903d0d7a8a34cd8db7c2d4aa8c9e5c659702eb
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-14 19:02:43 +0000

    perf: validador do Joi modificado pra ser generico
    A chave de mensagem do erro voltou a ser original do Joi
    para função ser usada em outros services

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,7 +1,7 @@
 const runSchema = (schema) => (data) => {
   const { error, value } = schema.validate(data);
   if (error) {
-    error.message = 'Some required fields are missing';
+    error.message = error.details[0].message;
     error.code = 400;
     throw error;
   }
