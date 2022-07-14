bdc21a34f9d07b5c6818948929fc3be019cebc89
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-14 20:02:34 -0300

    feat: usada função do jwt para validar token

--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -6,6 +6,17 @@ const jwtService = {
     const token = jwt.sign({ data }, process.env.JWT_SECRET);
     return token;
   },
+
+  validateToken: (token) => {
+    try {
+      const data = jwt.verify(token, process.env.JWT_SECRET);
+      return data;
+    } catch (e) {
+      const error = new Error('Expired or invalid token');
+      error.name = 'UnauthorizedError';
+      throw error;
+    }
+  },
 };
 
 module.exports = jwtService;
