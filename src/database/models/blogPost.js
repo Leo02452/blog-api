04c38c1b280952b36d94bfbeb2baf92ff0ba126a
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-15 19:41:01 -0300

    feat: criado modelo BlogPost

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,27 @@
+const createBlogPostModel = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'User',
+        key: 'id',
+      },
+    },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+}, {
+    timestamps: false,
+  });
+
+  return BlogPost;
+};
+
+module.exports = createBlogPostModel;
