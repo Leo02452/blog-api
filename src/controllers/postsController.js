7ccc7afa7f52ca9531bcbed7bc7d384fa19d5031
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-18 22:40:51 +0000

    feat: add controle pra criar post com categoria

---
+++ src/controllers/postsController.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+const config = require('../database/config/config');
+
+const sequelize = new Sequelize(config.development);
+
+const postsService = require('../services/postsService');
+const categoriesService = require('../services/categoriesService');
+const postCategoryService = require('../services/postCategoryService');
+
+const postsController = {
+  create: async (req, res) => {
+    const { user } = req;
+
+    const { title, content, categoryIds } = postsService.validateBody(req.body);
+
+    const categoryIdsPromises = categoryIds
+    .map((categoryId) => categoriesService.checkIfExists(categoryId));
+    await Promise.all(categoryIdsPromises);
+    
+    const newBlogPost = await sequelize.transaction(async (t) => {
+      const blogPostCreated = await postsService
+        .create({ title, content, userId: user.id }, { transaction: t });
+
+      const postCategoryPromises = categoryIds
+        .map((categoryId) => postCategoryService.create({ postId: blogPostCreated.id, categoryId }), { transaction: t })
+      await Promise.all(postCategoryPromises);
+
+      return blogPostCreated
+    });
+
+    res.status(201).json(newBlogPost);
+  },
+};
+
+module.exports = postsController;
